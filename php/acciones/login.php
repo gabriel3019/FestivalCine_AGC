6070a13f17bb9b93b13961be54bf8de69018b6c4
Gabriel <gabrielbr190@gmail.com>
2026-01-14 11:11:26 +0100

    Canditaturas en realización

--- php/acciones/login.php
+++ php/acciones/login.php
@@ -1,8 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 header('Content-Type: application/json');
-
 session_start();
 require "../BBDD/conecta.php";
 
--- php/acciones/noticiasOrganizador.php
+++ php/acciones/noticiasOrganizador.php
@@ -1,6 +1,4 @@
 <?php
-ini_set('display_errors', 0);
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 header('Content-Type: application/json');
 
 session_start();
